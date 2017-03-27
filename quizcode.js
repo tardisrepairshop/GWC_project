ec9faea0461716626d8a7890b7a7f28cebd52ffa
Bea D <bdergis@comcast.net>
2017-03-27 22:52:14 +0000

    I fixed the else if/if else thing

--- quizcode.js
+++ quizcode.js
@@ -1,4 +1,5 @@
 // JavaScript File
+/* global $ */
 $(".question").hide();
 $(".qOne").hide();
 $(".qgen").hide();
@@ -45,10 +46,10 @@ $(".start").click(function() {
                             if (fgans === "Mythology" || sfgans === "Mythology"){
                                 $("#results").append("Children's Mythology Books - Amazon: https://www.amazon.com/Best-Sellers-Books-Childrens-Folk-Tales-Myths/zgbs/books/2977/ref=zg_bs_unv_b_3_2990_1");
                             }
-                        } if else (age === "Teen/Young Adult"){
-                            
+                        } else if (age === "Teen/Young Adult"){
+                            $("#results").append("Teen Books - Amazon: https://www.amazon.com/b/ref=s9_acss_bw_en_BGG15eve_d_1_10_w?_encoding=UTF8&node=28&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-top-3&pf_rd_r=ZN3EEC2XT60XGX6D2W5A&pf_rd_r=ZN3EEC2XT60XGX6D2W5A&pf_rd_t=101&pf_rd_p=e8ce74da-9c3d-45b7-a3fd-ea13b4732f00&pf_rd_p=e8ce74da-9c3d-45b7-a3fd-ea13b4732f00&pf_rd_i=283155");
                         } else {
-                            
+                            $("#results").append("Amazon Books: https://www.amazon.com/books-used-books-textbooks/b?ie=UTF8&node=283155");
                         }
                         
                     });
